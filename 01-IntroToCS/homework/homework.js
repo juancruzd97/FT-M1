b4480fbaca91471fce76ff0b3314fd4c71924a98
JuanCruz <juancruzduarte1997@gmail.com>
2022-05-23 16:31:20 -0300

    FT-M1

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  var decimal = 0
+  for(var i = 0; i < num.length; i++){
+  decimal = decimal + num[i] * ( Math.pow(2, num.length - 1 - i))}
+  return decimal;
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  var binario = ''
+  while ( num > 0){              
+              
+    binario = (num % 2 ) + binario; 
+    num = Math.floor(num / 2)
+}
+return binario
+}
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
